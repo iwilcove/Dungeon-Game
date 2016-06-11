65fc890677b1dc5641279d8f3d22367d71e66026
Isaac <iwilcove@yahoo.com>
2016-06-11 10:38:38 -0700

    Added Comments to HappyEditorController

--- Assets/Scripts/HappyEnemyController.cs
+++ Assets/Scripts/HappyEnemyController.cs
@@ -12,20 +12,23 @@ public class HappyEnemyController : MonoBehaviour {
 	void Start() {
 
 		rb = GetComponent<Rigidbody>();
-		
+
 		player = GameObject.Find("Player");
 		pc = (PlayerController) player.GetComponent(typeof(PlayerController));
 
 	}
 
 	void Update() {
-		
+
+		// Calculate move distance
 		Transform target = pc.getPosition();
 		float step = speed * Time.deltaTime;
 
+		// Put move distance into effect
 		Vector3 posDifference = target.position - transform.position;
 		rb.AddForce (posDifference * speed);
 
+		// If the magnitude of velocity is over 2, it gets lowered
 		if (rb.velocity.magnitude > 2.0f) {
 			rb.AddForce (-1 * posDifference * speed);
 		}
